dcc71b643bb17dd07463b57add80e66be27b3d3c
alex.elrockero95 <alex.elrockero95@gmail.com>
2026-05-02 20:41:27 +0200

    footer.php

--- proyecto-php-motor/includes/footer.php
+++ proyecto-php-motor/includes/footer.php
@@ -1,6 +1,6 @@
 </main>
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> - Proyecto de Examen PHP. Creado por Arquitecto.</p>
+        <p>&copy; <?php echo date("Y"); ?> - Proyecto de taller creado por Alex.</p>
     </footer>
 </body>
 </html>
--- proyecto-php-motor/includes/header.php
+++ proyecto-php-motor/includes/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($pageTitle) ? $pageTitle : 'Examen PHP'; ?></title>
+    <title><?php echo isset($pageTitle) ? $pageTitle : 'TallerAlex PHP'; ?></title>
     
     <link rel="stylesheet" href="/css/style.css?v=<?php echo time(); ?>">
 </head>
