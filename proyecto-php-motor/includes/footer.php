0416d606e9f45323d445df3ac59c309a77890fb6
alex.elrockero95 <alex.elrockero95@gmail.com>
2026-05-02 20:40:38 +0200

    Creación de header y footer.php

---
+++ proyecto-php-motor/includes/footer.php
@@ -0,0 +1,7 @@
+</main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> - Proyecto de Examen PHP. Creado por Arquitecto.</p>
+    </footer>
+</body>
+</html>
+
---
+++ proyecto-php-motor/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo isset($pageTitle) ? $pageTitle : 'Examen PHP'; ?></title>
+    
+    <link rel="stylesheet" href="/css/style.css?v=<?php echo time(); ?>">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="galeria.php">Galería</a></li>
+            <li><a href="inventario.php">Inventario</a></li>
+            <li><a href="formulario.php">Formulario</a></li>
+        </ul>
+    </nav>
+    
+    <main>
+
